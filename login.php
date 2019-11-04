bba01cecc828b8d702eb8971c35063b4f80e966e
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-11-04 16:59:28 +0200

    start

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+
+<html>
+<head>
+	<meta http-equiv="refresh" content="5" content="width=device-width,initial-scale=1">
+	<title>Camagru-login page</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="box">
+		<form>
+			<div class="in-row">
+				<img src="https://media.thestar.com.my/Prod/E665221D-4BBC-44E9-993C-7901F463C663" style="width70" height="300">
+			</div>			
+				<h2 class="app-name">camagru<h2>
+				<input type="text"  name="name"placeholder="username or email" > <br>
+				<input type="text" name="password"placeholder="password" > <br>
+				<input type="button" value="Login">
+			</div>
+		</form>
+	</div>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+
+$words = 9;
+$string = "hey";
+
+for($count = 0; $count < $words; $count++)
+    echo '$string';
+?>
