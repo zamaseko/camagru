1a4ad5357e0af3817dd50f573d89881a63242080
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-11-13 18:32:13 +0200

    finally

--- signup.php
+++ signup.php
@@ -16,7 +16,7 @@
 </html>
 
 <?php
-
+include "./config/database.php";
 $server = 'localhost';
 $user = 'root';
 $db = 'camagru_db';
@@ -29,23 +29,22 @@ $lname = $_POST['sn'];
 $email = $_POST['e'];
 try
 {
-	$dsn = "mysql:host=$server;dbname=$db";
-	$connect = new pdo($dsn, $user, $password);
-	$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	if (empty($usrname) && empty($passwd) && empty($passwd2) && empty($email) && empty($fname) && empty($lname))
+	// $dsn = "mysql:host=$server;dbname=$db";
+	// $connect = new pdo($dsn, $user, $password);
+	// $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	if (!empty($usrname) && !empty($passwd) && !empty($passwd2) && !empty($email) && !empty($fname) && !empty($lname))
 	{
 		if(isset($usrname) && isset($passwd) && isset($passwd2) && isset($email) && isset($fname) && isset($lname))
 		{
 			$mys= $connect->query("SELECT username, email_address FROM users WHERE username=:username OR email_address=:email-address");
 			//$my = $connect->prepare('SELECT username, firstname, lastname, pass_word, email_address FROM users WHERE username = :username or email_address = :email_address');
-			$stmt = $connect->prepare($mysql);
+			$stmt = $connect->prepare($mys);
 			$stmt->execute(['username' => $usrname, 'firstname' => $fname, 'lastname' => $lname, 'pass_word' => $passwd, 'email_address' => $email]);
 			$usr = $stmt->fetch();
 			{
 				if($usr[1] != $usrname && $usr[5] != $email)
 				{
-					$email_add = filter_var($email_add, FILTER_SANITIZE_EMAIL);
-					if(filter_var($email_add, FILTER_SANITIZE_EMAIL))
+					if(filter_var(trim($email), FILTER_VALIDATE_EMAIL))
 					{
 						if(preg_match($passwd2, $passwd))
 						{
@@ -56,34 +55,52 @@ try
 						}
 						else
 						{
-							$pass = $usrname . $email_Add;
+							$pass = $usrname . $email;
 							$phash = md5($pass);
 							$email_cont = "Regitration for Camagru";
-							$head = "From noreply@camagruteam.co.za" . "\n";
-							$head .= 'MIME-Version: 1.0' . "\n";
-							$head .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+							$head = "From noreply@camagruteam.co.za" . "\r\n";
+							$head .= 'MIME-Version: 1.0' . "\r\n";
+							$head .= 'Content-type:text/html; 
+							charset=iso-8859-1' . "\r\n";
 							$content = "Welcome $fname $lname. <br> You have successfully signed up for Camagru. <br>
 							as a new member is simple to login. Just use $usrname and $passwd to login. <br>
 							The follwing email is to verify you as a member.Please click the link and follow to verify and
 							activate your account. <br>
-							Please check if this is you <br>	
-							
-							Username: $usrname <br>
-								If this is your chosen username please click the link below: <br>
-							<a href='http://localhost:8080/camagru/register.php?'>Click me </a><br><br>
+							Please check if this is you <br><br>
+							Username: $usrname <br><br>
+							If this is your chosen username please click the link below: <br>
+							<a href='http://localhost:8080/camagru/register.php?q=$phash'>Click me </a><br><br>
 							From: The Camagru team";
 					
-							$mysq = $connect->query('INSERT INTO users (username, firstname, lastname, pass_word, email_address) VALUES (?, ?, ?, ?, ?)');
-							$stmt = $connect->prepare($mysq);
-							$stmt->execute([$usrname, $fname,$lname, $passwd, $email_address]);
-							if (mail($email_add, $email_cont, $content, $head))
-							{
-								echo 'Verification email successfully received';
+							// $mysq = $connect->query('INSERT INTO users (username, firstname, lastname, pass_word, email_address) VALUES (?, ?, ?, ?, ?)');
+							// $stmt = $connect->prepare($mysq);
+							var_dump("we are here");
+
+							try {
+								$signup = $connect->prepare("INSERT INTO users(username, firstname, lastname, pass_word, email_address)VALUES (:username, :firstname, :lastname, :pass_word, :email_address)");
+								$signup->bindParam(':username', $usrname);
+								$signup->bindParam(':firstname', $fname);
+								$signup->bindParam(':lastname', $lname);
+								$signup->bindParam(':pass_word', $passwd);
+								$signup->bindParam(':email_address', $email);
+								$signup->execute();
+							} catch (Exception $e) {
+								echo $e->getMessage();
 							}
-							else 
-							{
-								echo 'There was an error, the email was not properly sent';
+							if ($signup) {
+								if (mail($email, $email_cont, $content, $head))
+								{
+									echo 'Verification email successfully received';
+								}
+								else 
+								{
+									echo 'There was an error, the email was not properly sent';
+								}
+							} else {
+								echo 'There was an error saving user to the database';
 							}
+							
+							
 						}	
 					}
 					else 
